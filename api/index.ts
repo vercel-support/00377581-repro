303ed3d09e9447b214ec3cd9e4c988202a252c22
mauricewegner <mauricewegner@icloud.com>
2024-09-19 14:18:14 +0200

    feat: add working endpoint

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,13 @@
-import { app } from '../core';
-import Environment from './_config';
+import { app } from '../core'
+import Environment from './_config'
 
 app.get('/', async (_, res) => {
-  const env = await Environment.find({});
-  res.status(200).json(env);
-});
+  const env = await Environment.find({})
+  res.status(200).json(env)
+})
 
-export default app;
+app.get('/working', (_, res) => {
+  res.status(200).json({ message: 'Working' })
+})
+
+export default app
